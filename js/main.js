b1b762cd23b4ec718d68c42a85d321c0a564a822
Felipe Silva <silvacontatoweb@gmail.com>
2021-03-26 12:54:07 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,182 @@
+(function($) {
+  
+  "use strict";
+  
+  /* Page Loader active
+  ========================================================*/
+  $('#preloader').fadeOut();
+
+  /* Testimonials Carousel 
+  ========================================================*/
+  var owl = $("#client-testimonial");
+    owl.owlCarousel({
+      navigation: true,
+      pagination: false,
+      slideSpeed: 1000,
+      stopOnHover: true,
+      autoPlay: true,
+      items: 1,
+      animateIn: 'fadeIn',
+      animateOut: 'fadeOut',
+      addClassActive: true,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [980,1],
+      itemsTablet: [768,1],
+      itemsTablet: [767,1],
+      itemsTabletSmall: [480,1],
+      itemsMobile : [479,1],
+    });   
+    $('#client-testimonial').find('.owl-prev').html('<i class="lni-chevron-left"></i>');
+    $('#client-testimonial').find('.owl-next').html('<i class="lni-chevron-right"></i>');
+
+
+    /* showcase Slider
+    =============================*/
+     var owl = $(".showcase-slider");
+      owl.owlCarousel({
+        navigation: false,
+        pagination: true,
+        slideSpeed: 1000,
+        margin:10,
+        stopOnHover: true,
+        autoPlay: true,
+        items: 5,
+        itemsDesktopSmall: [1024, 3],
+        itemsTablet: [600, 1],
+        itemsMobile: [479, 1]
+      });
+
+
+
+  /* 
+   Sticky Nav
+   ========================================================================== */
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 100) {
+            $('.header-top-area').addClass('menu-bg');
+        } else {
+            $('.header-top-area').removeClass('menu-bg');
+        }
+    });
+
+  /* 
+ VIDEO POP-UP
+ ========================================================================== */
+  $('.video-popup').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false,
+  });
+
+  /* 
+   Back Top Link
+   ========================================================================== */
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    })
+
+  /* 
+   One Page Navigation
+   ========================================================================== */
+
+
+    $(window).on('load', function() {
+       
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 100) {
+                $('.fixed-top').addClass('menu-bg');
+            } else {
+                $('.fixed-top').removeClass('menu-bg');
+            }
+        });
+
+    });
+
+  /* Auto Close Responsive Navbar on Click
+  ========================================================*/
+  function close_toggle() {
+      if ($(window).width() <= 768) {
+          $('.navbar-collapse a').on('click', function () {
+              $('.navbar-collapse').collapse('hide');
+          });
+      }
+      else {
+          $('.navbar .navbar-inverse a').off('click');
+      }
+  }
+  close_toggle();
+  $(window).resize(close_toggle);
+
+  /* Nivo Lightbox
+  ========================================================*/   
+   $('.lightbox').nivoLightbox({
+    effect: 'fadeScale',
+    keyboardNav: true,
+  });
+}(jQuery));
+
+// Animated word in header
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = ["inteligência.", "inovacação.", " impacto.", "tecnologia.", "resultados."];
+  
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.getElementById("head-title-animated").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
